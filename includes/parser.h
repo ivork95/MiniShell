34189dff2c41878d2d2cbad2a587ea7ea4252102
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-06-10 17:52:25 +0200

    modified parser prototype

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: kgajadie <kgajadie@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/10 15:22:11 by kgajadie      #+#    #+#                 */
-/*   Updated: 2022/06/10 15:22:40 by kgajadie      ########   odam.nl         */
+/*   Updated: 2022/06/10 17:36:36 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define PARSER_H
 
 int	closed_quotes(char *str);
+char **parser(char *line);
 
 #endif
