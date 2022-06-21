f913a2a15c04e811e3330b320adde7847740ae3f
Kawish Gajadien <kawishg@outlook.com>
2022-06-21 15:34:29 +0200

    feat: wip single quotes werkt

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: ivork <ivork@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/10 16:30:36 by ivork         #+#    #+#                 */
-/*   Updated: 2022/06/21 13:16:45 by kgajadie      ########   odam.nl         */
+/*   Updated: 2022/06/21 15:31:14 by kgajadie      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,38 @@ char **parser(const char *line)
     return (splitted_lines);
 }
 
+char *find_closing_single_quote(char *str, size_t *i)
+{
+    char *end = NULL;
+    while (str[*i])
+    {
+        if (str[*i + 1] == '\'')
+        { 
+            end = str + *i;
+            (*i)++;
+            break;
+        }
+        (*i)++;
+    }
+    return (end);
+}
+
+char *find_closing_double_quote(char *str, size_t *i)
+{
+    char *end = NULL;
+    while (str[*i])
+    {
+        if (str[*i + 1] == '\"')
+        { 
+            end = str + *i;
+            (*i)++;
+            break;
+        }
+        (*i)++;
+    }
+    return (end);
+}
+
 int main(void)
 {
     char *start = NULL;
@@ -61,20 +93,46 @@ int main(void)
     {
         if (str[i] == '\'')
         {
-            if (start == NULL)
-                start = str + i;
-            else
-                end = str + i;
-        }
-        if (start != NULL && end != NULL)
-        {
+            i++;
+            start = str + i;
+            end = find_closing_single_quote(str + i, &i);
             ft_strlcpy(ret, start, end - start + 2);
-            start = NULL;
-            end = NULL;
+            printf("ret = |%s|\n", ret);
         }
         i++;
     }
 
-    printf("ret = |%s|\n", ret);
     return (0);
 }
+
+// int main(void)
+// {
+//     char *start = NULL;
+//     char *end = NULL;
+//     char *str = "\'ls \' -la";
+//     char *ret = calloc(10, sizeof(*ret));
+//     size_t i = 0;
+
+//     while (str[i] != '\0')
+//     {
+//         if (str[i] == '\'')
+//         {
+//             if (start == NULL)
+//                 start = str + i;
+//             else
+//                 end = str + i;
+//         }
+//         if (start != NULL && end != NULL)
+//         {
+//             ft_strlcpy(ret, start, end - start + 2);
+//             start = NULL;
+//             end = NULL;
+//         }
+
+//         if (str[i] == '\'')
+//         i++;
+//     }
+
+//     printf("ret = |%s|\n", ret);
+//     return (0);
+// }
