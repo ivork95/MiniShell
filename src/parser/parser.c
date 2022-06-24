410b54f864e15118f7b844477eb1a7f72d6e8e6f
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-06-24 15:34:29 +0200

    added stdout test

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -41,39 +41,19 @@ char *find_closing_double_quote(char **str_dup)
 
 char *find_end_word(char **str_dup)
 {
-    char    *end;
-
-    end = NULL;
-    while (**str_dup != '\0')
-    {
-        if (**str_dup == ' ')
-            return (*str_dup);
+    while (**str_dup != '\0' && **str_dup != ' ')
         (*str_dup)++;
-    }
-    end = (*str_dup)--;
-    return (end);
+    return((*str_dup)--);
 }
 
 
 void print_arguments(char *str, long int length)
 {
-	int  i;
-
-	i = 0;
-	while(length > i)
-	{
-		printf("%c", str[i]);
-		i++;
-	}
-	printf("\n");
+    write(1, str, length);
 }
 
 void parser(const char* str)
 {
-    // char *ret_1 = calloc(10, sizeof(*ret_1));
-    // char *ret_2 = calloc(10, sizeof(*ret_2));
-    // char *ret_3 = calloc(10, sizeof(*ret_3));
-
     char    *start;
     char    *end;
     char    *str_dup;
@@ -90,9 +70,6 @@ void parser(const char* str)
             start = str_dup;
             end = find_closing_single_quote(&str_dup);
 			print_arguments(start, end - start);
-			// printf("%ld\n", end - start);
-            // ft_strlcpy(ret_1, start, end - start + 1);
-            // printf("ret_1 = |%s|\n", ret_1);
         }
         if (*str_dup == '\"')
         {
@@ -100,23 +77,12 @@ void parser(const char* str)
             start = str_dup;
             end = find_closing_double_quote(&str_dup);
 			print_arguments(start, end - start);
-            // ft_strlcpy(ret_2, start, end - start + 1);
-            // printf("ret_2 = |%s|\n", ret_2);
         }
         if (*str_dup != ' ')
         {
             start = str_dup;
             end = find_end_word(&str_dup);
-			print_arguments(start, end - start);
-            // ft_strlcpy(ret_3, start, end - start + 1);
-            // printf("ret_3 = |%s|\n", ret_3);
         }
         str_dup++;
     }
 }
-
-// int main(void)
-// {
-//     const char *str = "\'ls \'        \"-la \" grep    ";
-//     alpha(str);
-// }
