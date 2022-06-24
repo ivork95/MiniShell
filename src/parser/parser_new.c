9ef19f804e74f16deaa6fbca82ac27e2d22ce5c5
Kawish Gajadien <kawishg@outlook.com>
2022-06-24 15:40:20 +0200

    feat: wip refactor conditionals single - double - niet space

--- src/parser/parser_new.c
+++ src/parser/parser_new.c
@@ -9,15 +9,29 @@ typedef struct s_llnode
     struct s_llnode  *next;
 }   t_llnode;
 
-t_llnode    *create(char *str, t_llnode *old)
+void print_arguments(char *str, long int length)
 {
-    t_llnode    *new;
+	int  i;
 
-    new->str = malloc(strlen(str) * sizeof(*str));
-    if (new->str == NULL)
-        exit(EXIT_FAILURE);
-    new->next = old;
+	i = 0;
+    printf("|");
+	while(length > i)
+	{
+		printf("%c", str[i]);
+		i++;
+	}
+	printf("|\n");
 }
+
+// t_llnode    *create(char *str, t_llnode *old)
+// {
+//     t_llnode    *new;
+
+//     new->str = astr
+//     if (new->str == NULL)
+//         exit(EXIT_FAILURE);
+//     new->next = old;
+// }
 
 char *find_closing_single_quote(char **str_dup)
 {
@@ -55,68 +69,118 @@ char *find_closing_double_quote(char **str_dup)
     return (end);
 }
 
-char *find_end_word(char *str_dup)
+char *find_end_word(char **str_dup)
 {
     char    *end;
 
     end = NULL;
-    while (*str_dup != '\0')
+    while (**str_dup != '\0')
     {
-        if (*str_dup == ' ')
-            break;
-        str_dup++;
+        if (**str_dup == ' ')
+            return (*str_dup);
+        (*str_dup)++;
     }
-    end = str_dup--;
+    end = (*str_dup)--;
     return (end);
 }
 
-void alpha(const char* str)
+void handle_single_quotes(char **str_dup, t_llnode **head)
 {
-    char *ret_1 = calloc(10, sizeof(*ret_1));
-    char *ret_2 = calloc(10, sizeof(*ret_2));
-    char *ret_3 = calloc(10, sizeof(*ret_3));
+    char        *start;
+    char        *end;
+    char        *string;
+    t_llnode    *next;
 
+    (*str_dup)++;
+    start = *str_dup;
+    end = find_closing_single_quote(str_dup);
+    print_arguments(start, end - start);
+
+    next = malloc(sizeof(*next));
+    if (next == NULL)
+        exit(EXIT_FAILURE);
+    next->str = malloc(end - start + 1);
+    ft_strlcpy(next->str, start, end - start + 1);
+    next->next = *head;
+    *head = next;
+}
+
+void handle_double_quotes(char **str_dup, t_llnode **head)
+{
+    char        *start;
+    char        *end;
+    char        *string;
+    t_llnode    *next;
+
+    (*str_dup)++;
+    start = *str_dup;
+    end = find_closing_double_quote(str_dup);
+    print_arguments(start, end - start);
+
+    next = malloc(sizeof(*next));
+    if (next == NULL)
+        exit(EXIT_FAILURE);
+    next->str = malloc(end - start + 1);
+    ft_strlcpy(next->str, start, end - start + 1);
+    next->next = *head;
+    *head = next;
+}
+
+// void handle_double_quotes(char **str_dup)
+// {
+//     char    *start;
+//     char    *end;
+
+//     (*str_dup)++;
+//     start = *str_dup;
+//     end = find_closing_double_quote(str_dup);
+//     print_arguments(start, end - start);
+// }
+
+void handle_spaces(char **str_dup)
+{
     char    *start;
     char    *end;
+
+    start = *str_dup;
+    end = find_end_word(str_dup);
+    print_arguments(start, end - start);
+}
+
+void beta(t_llnode **head, char *str)
+{
     char    *str_dup;
 
-    start = NULL;
-    end = NULL;
     str_dup = str;
-
     while (*str_dup != '\0')
     {
         if (*str_dup == '\'')
-        {
-            str_dup++;
-            start = str_dup;
-            end = find_closing_single_quote(&str_dup);
-            ft_strlcpy(ret_1, start, end - start + 1);
-            printf("ret_1 = |%s|\n", ret_1);
-            printf("strdup_1 = |%s|\n", str_dup);
-        }
+            handle_single_quotes(&str_dup, head);
         if (*str_dup == '\"')
-        {
-            str_dup++;
-            start = str_dup;
-            end = find_closing_double_quote(&str_dup);
-            ft_strlcpy(ret_2, start, end - start + 1);
-            printf("ret_2 = |%s|\n", ret_2);
-        }
-        // if (*str_dup != ' ')
-        // {
-        //     str_dup++;
-        //     start = str_dup;
-        //     end = find_end_word(str_dup);
-        //     ft_strlcpy(ret_3, start, end - start + 1);
-        //     printf("ret_3 = |%s|\n", ret_3);
-        // }
+            handle_double_quotes(&str_dup, head);
+        if (*str_dup != ' ')
+            handle_spaces(&str_dup);
         str_dup++;
     }
 }
 
 int main(void)
 {
-    const char *str = "\'ls \' \"-la\" grep";
-    alpha(str);
+    char *str = "\'ls \' \"-la\" grep";
+
+    t_llnode *head;
+    head = malloc(sizeof(*head));
+    if (head == NULL)
+        exit(EXIT_FAILURE);
+    head->next = NULL;
+
+    printf("before = %p\n", head);
+    beta(&head, str);
+    printf("after = %p\n", head);
+
+    while (head->next != NULL)
+    {
+        printf("|%s|\n", head->str);
+        head = head->next;
+    }
 }
