606ee0758ed09071f0834b9a8e91bb251dac9a6f
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-07-27 19:04:57 +0200

    norminettw

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ivork <ivork@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/10 15:56:50 by ivork         #+#    #+#                 */
-/*   Updated: 2022/07/27 18:57:20 by ivork         ########   odam.nl         */
+/*   Updated: 2022/07/27 19:04:37 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,37 @@
 #include <stdlib.h>
 #include "../includes/parser.h"
 
-void print_commands(t_commands *cmds)
+void	print_commands(t_commands *cmds)
 {
+	size_t	i;
+
+	i = 0;
 	printf("--------------------\n");
 	printf("command = %s\n", cmds->cmd);
-	int i = 0;
 	while (cmds->args && cmds->args[i] != NULL)
 	{
 		printf("args = %s\n", cmds->args[i]);
 		i++;
 	}
 	if (cmds->files)
-		printf("file operattor = %d\nfile_name  = %s\n", cmds->files->type, cmds->files->file_name);
+	{
+		printf("file operattor = %d\nfile_name  = %s\n",
+			cmds->files->type, cmds->files->file_name);
+	}
 	printf("--------------------\n");
 }
 
-int	main()
+int	main(void)
 {
-	char		*user_input;
+	t_commands	*cmds;
 	t_tokens	*tokens;
-	t_commands 	*cmds;
-	
+	char		*user_input;
+
 	user_input = readline(">");
-	
 	tokens = tokenizer(user_input);
 	print_tokens(tokens);
-	
 	cmds = parser(tokens);
 	print_commands(cmds);
-	
 	free(user_input);
 	//todo free commands
 	return (0);
