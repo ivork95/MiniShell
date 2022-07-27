5451569a05cdbcf6fb4f8163479cee1048658967
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-07-27 19:08:54 +0200

    norminette

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: kgajadie <kgajadie@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/24 16:58:55 by kgajadie      #+#    #+#                 */
-/*   Updated: 2022/07/27 18:47:04 by ivork         ########   odam.nl         */
+/*   Updated: 2022/07/27 19:08:32 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	init_command(t_commands **command)
 	(*command)->next = NULL;
 }
 
-size_t redirect_type(char *str)
+size_t	redirect_type(char *str)
 {
 	if (ft_strncmp(str, "<", 2))
 		return (REDIRECT_IN);
@@ -32,11 +32,11 @@ size_t redirect_type(char *str)
 	return (-1);
 }
 
-void set_command(t_tokens **token, t_commands **command)
+void	set_command(t_tokens **token, t_commands **command)
 {
-	char *cmd;
+	char	*cmd;
 
-	cmd = malloc(sizeof(char*) * (*token)->len + 1);
+	cmd = malloc(sizeof(char *) * (*token)->len + 1);
 	if (cmd == NULL)
 	{
 		//todo exit mallc error
@@ -47,12 +47,11 @@ void set_command(t_tokens **token, t_commands **command)
 	*token = (*token)->next;
 }
 
-void set_args(t_tokens **token, t_commands **command)
+void	set_args(t_tokens **token, t_commands **command)
 {
-	t_tokens *tmp;
-	size_t i;
+	t_tokens	*tmp;
+	size_t		i;
 
-	
 	tmp = *token;
 	i = 0;
 	while (tmp && tmp->type == WORD)
@@ -61,7 +60,7 @@ void set_args(t_tokens **token, t_commands **command)
 		tmp = tmp->next;
 	}
 	printf("amount of args %ld\n", i);
-	(*command)->args = (char**)malloc(sizeof(char*) * i + 1);
+	(*command)->args = (char **)malloc(sizeof(char *) * i + 1);
 	if ((*command)->args == NULL)
 	{
 		// error check
@@ -72,33 +71,30 @@ void set_args(t_tokens **token, t_commands **command)
 	while ((*token) && (*token)->type == WORD)
 	{
 		printf("token str = %s\n", (*token)->str);
-		(*command)->args[i] = malloc(sizeof(char*) * (*token)->len + 1);
+		(*command)->args[i] = malloc(sizeof(char *) * (*token)->len + 1);
 		ft_strlcpy((*command)->args[i], (*token)->str, (*token)->len + 1);
 		*token = (*token)->next;
 		i++;
 	}
 }
 
-void set_files(t_tokens **token, t_commands **command)
+void	set_files(t_tokens **token, t_commands **command)
 {
 	t_files	*file;
-	
+
 	file = malloc(sizeof(t_files));
-	file->file_name = malloc(sizeof(char*) * (*token)->len + 1);
+	file->file_name = malloc(sizeof(char *) * (*token)->len + 1);
 	if (file->file_name == NULL)
 	{
 		//todo exit malloc error;
 		return ;
 	}
-	file->type= redirect_type((*token)->str);
+	file->type = redirect_type((*token)->str);
 	*token = (*token)->next;
 	ft_strlcpy(file->file_name, (*token)->str, (*token)->len + 1);
 	(*command)->files = file;
 	*token = (*token)->next;
 }
-
-// typedef int (*functions)(t_tokens *token, t_commands *commands);
-// functions func[3] = {&set_command, &set_files};
 
 void	fill_command(t_tokens **token, t_commands **command)
 {
@@ -110,9 +106,9 @@ void	fill_command(t_tokens **token, t_commands **command)
 		set_args(token, command);
 }
 
-t_commands *parser(t_tokens *token)
+t_commands	*parser(t_tokens *token)
 {
-	t_commands *command;
+	t_commands	*command;
 
 	command = malloc(sizeof(*command));
 	init_command(&command);
