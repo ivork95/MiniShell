7d639bbb3eb0355cfde9fbccee6cc7b118251b28
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-08-04 13:03:39 +0200

    initialized struct variables

--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: ivork <ivork@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/14 22:00:12 by ivork         #+#    #+#                 */
-/*   Updated: 2022/07/27 18:44:50 by ivork         ########   odam.nl         */
+/*   Updated: 2022/08/04 12:15:11 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ t_tokens	*create_new_token(void)
 		exit(EXIT_FAILURE);
 	new_token->str = NULL;
 	new_token->next = NULL;
+	new_token->quoted = false;
+	new_token->len = 0;
+	new_token->type = 0;
 	return (new_token);
 }
 
