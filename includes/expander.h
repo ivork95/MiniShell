e48ba5c69dc84c422deaec4a2ffa3db6b7a3ce1a
idan vork <idanvork@gmail.com>
2022-08-09 02:32:18 +0200

    expander

---
+++ includes/expander.h
@@ -0,0 +1,8 @@
+#ifndef EXPANDER_H
+# define EXPENDER_H
+
+# include "../includes/parser.h"
+
+t_command *expander(t_command *commands);
+
+#endif
---
+++ src/expander/expander.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   expander.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ivork <ivork@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/09 02:05:30 by ivork         #+#    #+#                 */
+/*   Updated: 2022/08/09 02:15:39 by ivork         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/parser.h"
+#include <stdlib.h>
+
+t_command *expander(t_command *commands)
+{
+    return (commands);
+}
