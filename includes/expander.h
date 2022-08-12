c1dd5bae5af0df331cfc3d3488500afef1d45025
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-08-12 13:18:57 +0200

    added struct

--- includes/expander.h
+++ includes/expander.h
@@ -3,6 +3,21 @@
 
 # include "../includes/parser.h"
 
-t_command *expander(t_command *commands);
+# define SINGLE_QUOTES 1
+# define DOUBLE_QUOTES 2
+
+typedef struct s_expand_data
+{
+	size_t len;
+	char *position_dollar_sign;
+	char *env_str;
+	char *env_name;
+	char *first_part_str;
+	char *last_part_str;
+	char *joined_str;
+	char *new_str;
+}	t_expand_data;
+
+void	expander(t_command *commands, char **envp);
 
 #endif
