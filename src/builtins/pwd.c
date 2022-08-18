249b4fd9f5140ee963ed1eacc2cdac7d8f1e62f3
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-08-18 17:56:34 +0200

    first version pwd

---
+++ src/builtins/pwd.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   pwd.c                                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ivork <ivork@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/18 15:37:58 by ivork         #+#    #+#                 */
+/*   Updated: 2022/08/18 16:02:00 by ivork         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_current_directory(void)
+{
+	char	*working_dir;
+	size_t	len;
+
+	working_dir = getcwd(NULL, 0);
+	write(1, working_dir, strlen(working_dir));
+	free(working_dir);
+}
