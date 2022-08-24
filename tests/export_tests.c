b1ee2347c839db83e4156c943c9b235226baf545
Kawish Gajadien <kawishg@outlook.com>
2022-08-24 15:28:22 +0200

    feat: pas struct voor export built in aan feat: voeg code toe om env te splitten met strchr

--- tests/export_tests.c
+++ tests/export_tests.c
@@ -9,93 +9,106 @@ extern char **environ;
 
 typedef struct s_env_var
 {
-	char *env_var;
-	struct s_env_var *next;
+	char				*key;
+	char				*value;
+	struct s_env_var	*next;
 }	t_env_var;
+
+void	print_env_vars(t_env_var *head)
+{
+	while (head != NULL)
+	{
+		printf("%s=%s\n", head->key, head->value);
+		head = head->next;
+	}
+	printf("\n");
+}
 
 void	free_env_vars(t_env_var *head)
 {
 	if (head->next)
 		free_env_vars(head->next);
-	free(head->env_var);
+	free(head->key);
+	free(head->value);
 	free(head);
 }
 
-int count_env_vars(char **environ)
-{
-	int	i;
-
-	i = 0;
-	while (environ[i] != NULL)
-		i++;
-	return (i);
-}
-
-t_env_var	*add_env_var(char *content)
+t_env_var	*add_env_var(char *key, char *value)
 {
 	t_env_var	*node;
 
 	node = malloc(sizeof(*node));
 	if (node == NULL)
 		exit(EXIT_FAILURE);
-	node->env_var = ft_strdup(content);
-	node->next = NULL;
-	if (node->env_var == NULL)
+	node->key = ft_strdup(key);
+	if (node->key == NULL)
 		exit(EXIT_FAILURE);
+	node->value = ft_strdup(value);
+	if (node->value == NULL)
+		exit(EXIT_FAILURE);
+	node->next = NULL;
 	return (node);
-}
-
-t_env_var	*environ_to_linked_list(char** environ)
-{
-	int			i;
-	int			number_of_nodes;
-	t_env_var	*head;
-	t_env_var	*node;
-
-	i = 0;
-	head = NULL;
-	number_of_nodes = count_env_vars(environ);
-	while (i < number_of_nodes)
-	{
-		node = add_env_var(environ[i]);
-		if (head == NULL)
-			head = node;
-		else
-		{
-			node->next = head;
-			head = node;
-		}
-		i++;
-	}
-	return (head);
 }
 
 t_env_var *environ_to_linked_list_recursive(t_env_var *head, char** environ)
 {
+	char	**env_var;
+	char *p;
+
+	head = NULL;
 	if (*environ != NULL)
 	{
 		head = malloc(sizeof(*head));
 		if (head == NULL)
 			exit(EXIT_FAILURE);
-		head->env_var = ft_strdup(*environ);
-		if (head->env_var == NULL)
+
+		// PWD=/pwd/tests
+		p = ft_strchr(*environ, '=');
+		head->key = malloc(sizeof(*(head->key)) * ((p - *environ) + 1));
+		if (head->key == NULL)
 			exit(EXIT_FAILURE);
-		head->next = NULL;
+		ft_strlcpy(head->key, *environ, (p - *environ) + 1);
+
+		p = p + 1;
+		head->value = malloc(sizeof(*(head->value)) * (ft_strlen(p) + 1));
+		if (head->value == NULL)
+			exit(EXIT_FAILURE);
+		ft_strlcpy(head->value, p, ft_strlen(p) + 1);
+
+		// env_var = ft_split(*environ, '=');
+		// if (env_var == NULL)
+		// 	exit(EXIT_FAILURE);
+		// head->key = env_var[0];
+		// head->value = env_var[1];
+		// free(env_var);
 		head->next = environ_to_linked_list_recursive(head->next, environ + 1);
 	}
 	return (head);
 }
 
-void	print_env_vars(t_env_var *head)
+int main(void)
 {
-	while (head != NULL)
-	{
-		printf("|%s|\n", head->env_var);
-		head = head->next;
-	}
-	printf("\n");
-}
+	// int n = get_number_of_nodes(environ);
+	// printf("n = %i\n", n);
 
+	// t_env_var *head;
+	// head = NULL;
+	// head = environ_to_linked_list(environ);
+	// print_env_vars(head);
+
+	// t_env_var *new = add_env_var("jon=aegon");
+	// new->next = head;
+	// head = new;
+	// print_env_vars(head);
+
+	t_env_var *head;
+	head = NULL;
+	head = environ_to_linked_list_recursive(head, environ);
+	print_env_vars(head);
+
+	free_env_vars(head);
+	return (0);
+}
 
 // zet env om in linked list
 // hoeveel nodes heb ik nodig
@@ -137,28 +150,3 @@ void	print_env_vars(t_env_var *head)
 //         i++;
 //     }
 // }
-
-
-int main(void)
-{
-	// int n = get_number_of_nodes(environ);
-	// printf("n = %i\n", n);
-
-	t_env_var *head;
-	head = NULL;
-	head = environ_to_linked_list(environ);
-	print_env_vars(head);
-
-	// t_env_var *new = add_env_var("jon=aegon");
-	// new->next = head;
-	// head = new;
-	// print_env_vars(head);
-
-	// t_env_var *head;
-	// head = NULL;
-	// head = environ_to_linked_list_recursive(head, environ);
-	// print_env_vars(head);
-
-	free_env_vars(head);
-	return (0);
-}
