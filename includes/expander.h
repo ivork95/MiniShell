68027bf9ad127a89c8930d4bdd3e65491f51bab0
idan vork <idanvork@gmail.com>
2022-09-25 14:55:11 +0200

    new expander logic

--- includes/expander.h
+++ includes/expander.h
@@ -6,7 +6,7 @@
 /*   By: ivork <ivork@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/26 14:02:50 by ivork         #+#    #+#                 */
-/*   Updated: 2022/09/23 13:56:53 by kgajadie      ########   odam.nl         */
+/*   Updated: 2022/09/25 02:47:12 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,6 @@ size_t			get_len_place_holder(char *str);
 size_t			count_letters(char *str, char delimiter);
 size_t			check_quote_type(char *str);
 void			null_data(t_expand_data *data);
-t_expand_data	set_data(t_expand_data data, char *str);
+t_expand_data	set_data(t_expand_data data, char *str, char *pos_dollar_sign, t_env_var *envp);
 void			free_expand_data(t_expand_data *data);
 #endif
