ddcb0033ed1d1eca1d341e81ccdebac85ae85b7f
Kawish Gajadien <kawishg@outlook.com>
2022-09-26 14:25:33 +0200

    fix: wip maak free command functies niet recursief en zet malloc op 100 om leak makkelijker te kunnen vinden

--- src/parser/free_breezy.c
+++ src/parser/free_breezy.c
@@ -6,36 +6,77 @@
 /*   By: ivork <ivork@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/26 12:32:27 by ivork         #+#    #+#                 */
-/*   Updated: 2022/09/23 14:29:11 by kgajadie      ########   odam.nl         */
+/*   Updated: 2022/09/26 13:59:28 by kawish        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/parser.h"
 
+// void	free_files(t_file *head)
+// {
+// 	if (head->next)
+// 		free_files(head->next);
+// 	free(head->file_name);
+// 	free(head);
+// }
+
+// void	free_commands(t_command *head)
+// {
+// 	size_t	i;
+
+// 	if (head->next)
+// 		free_commands(head->next);
+// 	i = 0;
+// 	while (head->args[i] != NULL)
+// 	{
+// 		free(head->args[i]);
+// 		i++;
+// 	}
+// 	free(head->args);
+// 	if (head->files)
+// 		free_files(head->files);
+// 	free(head);
+// }
+
 void	free_files(t_file *head)
 {
-	if (head->next)
-		free_files(head->next);
-	free(head->file_name);
-	free(head);
+	t_file	*tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->file_name);
+		free(tmp);
+	}
+}
+
+void	free_args(char **args)
+{
+	unsigned int	i;
+
+	i = 1;
+	while (args[i] != NULL)
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
 }
 
 void	free_commands(t_command *head)
 {
-	size_t	i;
+	t_command	*tmp;
 
-	if (head->next)
-		free_commands(head->next);
-	i = 0;
-	while (head->args[i] != NULL)
+	while (head != NULL)
 	{
-		free(head->args[i]);
-		i++;
+		tmp = head;
+		head = head->next;
+		free(tmp->cmd);
+		free_args(tmp->args);
+		free_files(tmp->files);
+		free(tmp);
 	}
-	free(head->args);
-	if (head->files)
-		free_files(head->files);
-	free(head);
 }
 
 void	free_splitted_array(char **splitted_array)
