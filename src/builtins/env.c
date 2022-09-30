d17ce06da208d68dabb1c40a291e95650d32c231
Kawish Gajadien <kawishg@outlook.com>
2022-09-30 17:02:23 +0200

    feat: haal /bin/zsh env weg uit env

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: kawish <kawish@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/31 17:17:30 by kawish        #+#    #+#                 */
-/*   Updated: 2022/09/22 16:04:24 by kgajadie      ########   odam.nl         */
+/*   Updated: 2022/09/30 16:59:04 by kgajadie      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,16 @@ void	free_env_vars(t_env_var *head)
 	}
 }
 
-void	env(__attribute__ ((unused)) t_command *command, t_env_var **head)
+void	env(t_command *command, t_env_var **head)
 {
 	t_env_var	*vars;
 
 	vars = *head;
 	while (vars != NULL)
 	{
-		if (ft_strncmp(vars->key, "?", 2))
-		{
-			ft_putstr_fd(vars->key, 1);
-			ft_putchar_fd('=', 1);
-			ft_putendl_fd(vars->value, 1);
-		}
+		ft_putstr_fd(vars->key, 1);
+		ft_putchar_fd('=', 1);
+		ft_putendl_fd(vars->value, 1);
 		vars = vars->next;
 	}
 }
@@ -84,6 +81,5 @@ t_env_var	*environ_to_linked_list_recursive(t_env_var *head, char **environ)
 					environ + 1);
 		}
 	}
-	add_env_var(&head, "?=0");
 	return (head);
 }
