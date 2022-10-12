84af34a93c0d699496591975cb39916bd1b66162
Kawish Gajadien <kawishg@outlook.com>
2022-10-12 11:54:19 +0200

    fix: pas exit.c aan zodat ie tests passed

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: kawish <kawish@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/06 17:28:45 by kawish        #+#    #+#                 */
-/*   Updated: 2022/10/06 14:22:48 by kgajadie      ########   odam.nl         */
+/*   Updated: 2022/10/12 11:48:33 by kgajadie      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,28 @@ extern int	g_exit_status;
 
 static bool	str_is_numeric(char *s)
 {
-	char	*ascii;
-
+	char			*ascii;
+	unsigned int	i;
+	
+	i = 0;
+	if (*s == '-')
+		s++;
+	while (s[i])
+	{
+		if (!ft_isdigit(s[i]))
+			return (false);
+		i++;
+	}
 	ascii = ft_itoa(ft_atoi(s));
 	if (ascii == NULL)
 		perror_and_exit("malloc", EXIT_FAILURE);
-	if (ft_strlen(s) == ft_strlen(ascii))
+	if (ft_strlen(s) != ft_strlen(ascii))
 	{
 		free(ascii);
-		return (true);
+		return (false);
 	}
 	free(ascii);
-	return (false);
+	return (true);
 }
 
 static void	handle_exit_multiple_args(char **cmd_args, pid_t cpid)
@@ -54,7 +64,7 @@ static void	handle_exit_multiple_args(char **cmd_args, pid_t cpid)
 			if (cpid != 0)
 				ft_putendl_fd("exit", STDOUT_FILENO);
 			ft_putendl_fd("minishell: exit: too many arguments", STDOUT_FILENO);
-			g_exit_status = 2;
+			g_exit_status = 1;
 		}
 	}
 }
