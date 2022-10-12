71f766f1ee396f3c0c9690f5b751d0db7a598d9e
Kawish Gajadien <kawishg@outlook.com>
2022-10-12 11:53:25 +0200

    feat: voeg echo tests toe

--- tests/integration_tests/echo.c
+++ tests/integration_tests/echo.c
@@ -21,60 +21,70 @@ static void	setup(void)
 /* Echo */
 Test(echo, echo_hello_world, .init=setup)
 {
-	char *inputs[] = {
+	char *test_inputs[] = {
 		"echo Hello, World!",
 		0
 	};
-
-	minicore(inputs, onze_env);
-
+	minicore(test_inputs, onze_env);
 	cr_assert_stdout_eq_str("Hello, World!\n");
 }
 
 Test(echo, echo_n_hello_world, .init=setup)
 {
-	char *inputs[] = {
+	char *test_inputs[] = {
 		"echo -n Hello, World!",
 		0
 	};
-
-	minicore(inputs, onze_env);
-
+	minicore(test_inputs, onze_env);
 	cr_assert_stdout_eq_str("Hello, World!");
 }
 
 Test(echo, echo, .init=setup)
 {
-	char *inputs[] = {
+	char *test_inputs[] = {
 		"echo",
 		0
 	};
-
-	minicore(inputs, onze_env);
-
+	minicore(test_inputs, onze_env);
 	cr_assert_stdout_eq_str("\n");
 }
 
 Test(echo, echo_n, .init=setup)
 {
-	char *inputs[] = {
+	char *test_inputs[] = {
 		"echo -n",
 		0
 	};
-
-	minicore(inputs, onze_env);
-
+	minicore(test_inputs, onze_env);
 	cr_assert_stdout_eq_str("");
+}
+
+Test(echo, echo_$_question_mark, .init=setup)
+{
+	char *test_inputs[] = {
+		"echo $?",
+		0
+	};
+	minicore(test_inputs, onze_env);
+	cr_assert_stdout_eq_str("0\n");
 }
 
 Test(echo, echo_$, .init=setup)
 {
-	char *inputs[] = {
-		"echo $?",
+	char *test_inputs[] = {
+		"echo $",
 		0
 	};
+	minicore(test_inputs, onze_env);
+	cr_assert_stdout_eq_str("$\n");
+}
 
-	minicore(inputs, onze_env);
-
-	cr_assert_stdout_eq_str("0\n");
+Test(echo, echo_$$, .init=setup)
+{
+	char *test_inputs[] = {
+		"echo $$",
+		0
+	};
+	minicore(test_inputs, onze_env);
+	cr_assert_stdout_eq_str("Should echo current PID\n");
 }
