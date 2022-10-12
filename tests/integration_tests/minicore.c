9f093168059326207647c8b33b44b9e487a2da88
Kawish Gajadien <kawishg@outlook.com>
2022-10-12 11:52:57 +0200

    chore: verwijder comment

--- tests/integration_tests/minicore.c
+++ tests/integration_tests/minicore.c
@@ -35,49 +35,6 @@ char	*two_d_to_str(char **environ)
 	return (a);
 }
 
-// void minicore(char **inputs, t_env_var *onze_env)
-// {
-// 	t_command	*cmds;
-// 	t_token		*tokens;
-// 	char		*user_input;
-
-// 	g_exit_status = 0;
-// 	while (*inputs)
-// 	{
-// 		user_input = ft_strdup(*inputs);
-// 		tokens = tokenizer(user_input);
-// 		if (tokens == NULL)
-// 		{
-// 			free(user_input);
-// 			free_tokens(tokens);
-
-// 			inputs++;
-
-// 			continue;
-// 		}
-// 		cmds = parser(tokens, &onze_env);
-// 		expander(cmds, onze_env);
-// 		if (cmds->cmd[0] == 0)
-// 		{
-// 			free(user_input);
-// 			free_tokens(tokens);
-// 			free_commands(cmds);
-
-// 			inputs++;
-
-// 			continue;
-// 		}
-// 		executor(cmds, &onze_env);
-// 		add_history(user_input);
-// 		free(user_input);
-// 		free_tokens(tokens);
-// 		free_commands(cmds);
-
-// 		inputs++;
-// 	}
-// 	free_env_vars(onze_env);
-// }
-
 static void	get_user_input(char **user_input)
 {
 	struct sigaction	sa;
